d1afbbb855f5d0c3b6eede31823737b784bc42c1
katja tsareva <katja.tcareva@gmail.com>
2025-09-18 14:12:30 +0200

    Rewrite seeder as separate services, add seeder for Courses

--- Backend/LMS.API/Services/DataSeedHostingService.cs
+++ Backend/LMS.API/Services/DataSeedHostingService.cs
@@ -1,28 +1,15 @@
-using Bogus;
-using LMS.Infractructure.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
+using LMS.API.Services.Seed;
 
 namespace LMS.API.Services;
 
-//Add in secret.json
-//{
-//   "password" :  "YourSecretPasswordHere"
-//}
 public class DataSeedHostingService : IHostedService
 {
     private readonly IServiceProvider serviceProvider;
-    private readonly IConfiguration configuration;
     private readonly ILogger<DataSeedHostingService> logger;
-    private UserManager<ApplicationUser> userManager = null!;
-    private RoleManager<IdentityRole> roleManager = null!;
-    private const string TeacherRole = "Teacher";
-    private const string StudentRole = "Student";
 
-    public DataSeedHostingService(IServiceProvider serviceProvider, IConfiguration configuration, ILogger<DataSeedHostingService> logger)
+    public DataSeedHostingService(IServiceProvider serviceProvider, ILogger<DataSeedHostingService> logger)
     {
         this.serviceProvider = serviceProvider;
-        this.configuration = configuration;
         this.logger = logger;
     }
 
@@ -33,20 +20,10 @@ public class DataSeedHostingService : IHostedService
         var env = scope.ServiceProvider.GetRequiredService<IWebHostEnvironment>();
         if (!env.IsDevelopment()) return;
 
-        var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-        if (await context.Users.AnyAsync(cancellationToken)) return;
-
-        userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
-        roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-        ArgumentNullException.ThrowIfNull(roleManager, nameof(roleManager));
-        ArgumentNullException.ThrowIfNull(userManager, nameof(userManager));
-
         try
         {
-            await AddRolesAsync([TeacherRole, StudentRole]);
-            await AddDemoUsersAsync();
-            await AddUsersAsync(20);
+            var seeder = scope.ServiceProvider.GetRequiredService<DataSeeder>();
+            await seeder.SeedAsync(cancellationToken);
             logger.LogInformation("Seed complete");
         }
         catch (Exception ex)
@@ -56,62 +33,5 @@ public class DataSeedHostingService : IHostedService
         }
     }
 
-    private async Task AddRolesAsync(string[] rolenames)
-    {
-        foreach (string rolename in rolenames)
-        {
-            if (await roleManager.RoleExistsAsync(rolename)) continue;
-            var role = new IdentityRole { Name = rolename };
-            var res = await roleManager.CreateAsync(role);
-
-            if (!res.Succeeded) throw new Exception(string.Join("\n", res.Errors));
-        }
-    }
-    private async Task AddDemoUsersAsync()
-    {
-        var teacher = new ApplicationUser
-        {
-            UserName = "teacher@test.com",
-            Email = "teacher@test.com"
-        };
-
-        var student = new ApplicationUser
-        {
-            UserName = "student@test.com",
-            Email = "student@test.com"
-        };
-
-        await AddUserToDb([teacher, student]);
-
-        var teacherRoleResult = await userManager.AddToRoleAsync(teacher, TeacherRole);
-        if (!teacherRoleResult.Succeeded) throw new Exception(string.Join("\n", teacherRoleResult.Errors));
-
-        var studentRoleResult = await userManager.AddToRoleAsync(student, StudentRole);
-        if (!studentRoleResult.Succeeded) throw new Exception(string.Join("\n", studentRoleResult.Errors));
-    }
-
-    private async Task AddUsersAsync(int nrOfUsers)
-    {
-        var faker = new Faker<ApplicationUser>("sv").Rules((f, e) =>
-        {
-            e.Email = f.Person.Email;
-            e.UserName = f.Person.Email;
-        });
-
-        await AddUserToDb(faker.Generate(nrOfUsers));
-    }
-
-    private async Task AddUserToDb(IEnumerable<ApplicationUser> users)
-    {
-        var passWord = configuration["password"];
-        ArgumentNullException.ThrowIfNull(passWord, nameof(passWord));
-
-        foreach (var user in users)
-        {
-            var result = await userManager.CreateAsync(user, passWord);
-            if (!result.Succeeded) throw new Exception(string.Join("\n", result.Errors));
-        }
-    }
     public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
-
 }
