6efd1cc5ecfbc44a3ee9e4ef5fd45caaf0b52d55
wpplng <lina.wappling@gmail.com>
2025-09-19 12:02:21 +0200

    Created app bar with name, role and log out btn

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,9 +1,20 @@
 import { ReactElement } from 'react';
 import { useAuthContext } from '../utilities/hooks/useAuthContext';
 import { useNavigate } from 'react-router';
+import { AppBar, Box, Button, Chip, Container, Toolbar, Typography, styled } from '@mui/material';
+import LogoutIcon from '@mui/icons-material/Logout';
+import decodeToken from '../utilities/token/decodeToken';
+import { getTokens } from '../utilities/token';
+
+const StyledAppBar = styled(AppBar)(() => ({
+  backgroundColor: '#ffffff',
+}));
 
 const Header = (): ReactElement => {
   const { isLoggedIn, logout } = useAuthContext();
+  const token = getTokens();
+  const { name, role } = decodeToken(token ? token.accessToken : '');
+
   const navigate = useNavigate();
 
   const handleOnLogout = () => {
@@ -12,10 +23,23 @@ const Header = (): ReactElement => {
   };
 
   return (
-    <header className="g-container" id="header">
-      <h1>Companies API</h1>
-      {isLoggedIn && <button onClick={handleOnLogout}>Logout</button>}
-    </header>
+    <StyledAppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+            {isLoggedIn && (
+              <>
+                <Typography sx={{ paddingRight: 2, color: 'text.primary' }}>{name}</Typography>
+                <Chip label={role} sx={{ backgroundColor: '#DBEAFE', color: '#1E40AF', marginRight: 2 }} />
+                <Button onClick={handleOnLogout} sx={{ color: 'text.primary' }}>
+                  <LogoutIcon sx={{ marginRight: 1 }} /> Logga ut
+                </Button>
+              </>
+            )}
+          </Box>
+        </Toolbar>
+      </Container>
+    </StyledAppBar>
   );
 };
 
