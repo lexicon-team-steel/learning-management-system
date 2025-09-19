0df6eb149b4f1e19d670eb928f7c73a9eed02863
Nattarintra Thutsanti <nattarintra@headofsearch.se>
2025-09-19 15:28:09 +0700

    Refactor the container’s styled component

--- Frontend/src/components/Main.tsx
+++ Frontend/src/components/Main.tsx
@@ -1,11 +1,21 @@
+import { styled } from '@mui/material';
 import Container from '@mui/material/Container';
 import { ReactElement, ReactNode } from 'react';
+
 interface IMainProps {
   children: ReactNode;
 }
 
+const StyledContainer = styled(Container)(() => ({
+  padding: '1.5rem',
+}));
+
 const Main = ({ children }: IMainProps): ReactElement => {
-  return <Container style={{ padding: 24 }}>{children}</Container>;
+  return (
+    <StyledContainer>
+      <Container component="main">{children}</Container>
+    </StyledContainer>
+  );
 };
 
 export default Main;
