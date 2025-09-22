61bf3620c4e97c0583fedfc70ccbdb53637ee17e
katja tsareva <katja.tcareva@gmail.com>
2025-09-22 14:36:12 +0200

    Create ParticipantItem component

---
+++ Frontend/src/features/course-participants/components/ParticipantItem.tsx
@@ -0,0 +1,38 @@
+import { ReactElement } from 'react';
+import { Participant } from '../types';
+import { Avatar, ListItem, ListItemAvatar, ListItemText, styled } from '@mui/material';
+import colors from '../../../styles/colors';
+
+interface IParticipantItemProps {
+  participant: Participant;
+}
+
+const StyledAvatar = styled(Avatar)(() => ({
+  backgroundColor: colors.primaryBlue,
+}));
+
+const StyledListItem = styled(ListItem)(() => ({
+  paddingLeft: 0,
+  paddingRight: 0,
+}));
+
+const StyledListItemText = styled(ListItemText)({
+  '& .MuiListItemText-primary': {
+    fontWeight: '500',
+  },
+});
+
+const ParticipantItem = ({ participant }: IParticipantItemProps): ReactElement => {
+  const firstLetter = participant.fullname.charAt(0);
+
+  return (
+    <StyledListItem>
+      <ListItemAvatar>
+        <StyledAvatar>{firstLetter}</StyledAvatar>
+      </ListItemAvatar>
+      <StyledListItemText primary={participant.fullname} secondary={participant.email} />
+    </StyledListItem>
+  );
+};
+
+export default ParticipantItem;
