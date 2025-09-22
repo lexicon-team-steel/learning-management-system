03e82f7f280b0e47e3d5aa47cb33dfbfcf987306
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-22 15:15:29 +0200

    Add styling to form and book icon

--- Frontend/src/components/Login.tsx
+++ Frontend/src/components/Login.tsx
@@ -1,8 +1,27 @@
 import { FormEventHandler, ReactElement, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
 import { useAuthContext } from '../utilities/hooks/useAuthContext';
-import { Box, Button, TextField } from '@mui/material';
+import { Button, styled, TextField } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Logout';
+import ImportContactsOutlinedIcon from '@mui/icons-material/ImportContactsOutlined';
+import theme from '../styles/theme';
+
+const StyledForm = styled('form')(() => ({
+  backgroundColor: theme.palette.background.paper,
+  borderRadius: theme.shape.borderRadius,
+  maxWidth: '450px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  gap: theme.spacing(3),
+  padding: theme.spacing(4),
+  width: '100%',
+  boxShadow: '0 2px 3px 2px #b2b6c2ff',
+}));
+
+const StyledIcon = styled(ImportContactsOutlinedIcon)(() => ({
+  fontSize: '60px',
+}));
 
 const Login = (): ReactElement => {
   const [username, setUsername] = useState<string>('');
@@ -21,30 +40,30 @@ const Login = (): ReactElement => {
   };
 
   return (
-    <form onSubmit={handleOnSubmit}>
-      <Box display="flex" flexDirection="column" alignItems="center" gap={2} maxWidth={450}>
-        <TextField
-          label="Användarnamn"
-          name="username"
-          variant="outlined"
-          onChange={(e) => setUsername(e.target.value)}
-          fullWidth
-          required
-        />
-        <TextField
-          label="Lösenord"
-          name="password"
-          type="password"
-          variant="outlined"
-          onChange={(e) => setPassword(e.target.value)}
-          fullWidth
-          required
-        />
-        <Button type="submit" variant="contained" startIcon={<LoginIcon />} fullWidth>
-          Logga in
-        </Button>
-      </Box>
-    </form>
+    <StyledForm onSubmit={handleOnSubmit}>
+      <StyledIcon color="primary" />
+      <h1>Steel LMS</h1>
+      <TextField
+        label="Användarnamn"
+        name="username"
+        variant="outlined"
+        onChange={(e) => setUsername(e.target.value)}
+        fullWidth
+        required
+      />
+      <TextField
+        label="Lösenord"
+        name="password"
+        type="password"
+        variant="outlined"
+        onChange={(e) => setPassword(e.target.value)}
+        fullWidth
+        required
+      />
+      <Button type="submit" variant="contained" startIcon={<LoginIcon />} fullWidth>
+        Logga in
+      </Button>
+    </StyledForm>
   );
 };
 
