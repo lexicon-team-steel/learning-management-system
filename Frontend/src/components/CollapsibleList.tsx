67eb3800582b1828441bebe55c9899f5c0f135b9
katja tsareva <katja.tcareva@gmail.com>
2025-09-22 19:10:28 +0200

    Create simple generic CollapsibleList component

---
+++ Frontend/src/components/CollapsibleList.tsx
@@ -0,0 +1,48 @@
+import { Fragment, ReactElement, ReactNode } from 'react';
+import { Button, Divider, List, SxProps } from '@mui/material';
+import { useCollapse } from '../utilities/hooks/useCollapsible';
+
+interface ICollapsibleListProps<T> {
+  items: T[];
+  renderItem: (i: T) => ReactNode;
+  keyField: keyof T;
+  itemsDefault?: number;
+  isCollapsed?: boolean;
+  hasDivider?: boolean;
+  showMoreLabel?: string;
+  showLessLabel?: string;
+  sx?: SxProps;
+}
+
+const CollapsibleList = <T,>({
+  items,
+  renderItem,
+  keyField,
+  itemsDefault = 5,
+  isCollapsed = true,
+  hasDivider = true,
+  showMoreLabel = 'Visa fler',
+  showLessLabel = 'Visa mindre',
+  sx,
+}: ICollapsibleListProps<T>): ReactElement => {
+  const { collapsed, toggleCollapse } = useCollapse(isCollapsed);
+  const visibleItems = collapsed ? items.slice(0, itemsDefault) : items;
+  const buttonTitle = collapsed ? showMoreLabel : showLessLabel;
+  const showDivider = (idx: number) => hasDivider && idx < visibleItems.length - 1 && <Divider />;
+
+  return (
+    <>
+      <List sx={sx}>
+        {visibleItems.map((item, idx) => (
+          <Fragment key={item[keyField] as string}>
+            {renderItem(item)}
+            {showDivider(idx)}
+          </Fragment>
+        ))}
+      </List>
+      {items.length > itemsDefault && <Button onClick={toggleCollapse}>{buttonTitle}</Button>}
+    </>
+  );
+};
+
+export default CollapsibleList;
