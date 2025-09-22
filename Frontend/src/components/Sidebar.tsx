70bb99cb3637385ef9b150d171a1eab45563589f
wpplng <lina.wappling@gmail.com>
2025-09-22 16:13:34 +0200

    Moved nav items to constants file

--- Frontend/src/components/Sidebar.tsx
+++ Frontend/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from 'react';
-import { useNavigate, useLocation, NavigateFunction } from 'react-router';
+import { useNavigate, useLocation } from 'react-router';
 import {
   Box,
   Drawer,
@@ -11,21 +11,11 @@ import {
   Typography,
   styled,
 } from '@mui/material';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import ImportContactsOutlinedIcon from '@mui/icons-material/ImportContactsOutlined';
-import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
-import EventNoteOutlinedIcon from '@mui/icons-material/EventNoteOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import colors from '../styles/colors';
 import decodeToken from '../utilities/token/decodeToken';
 import { getTokens } from '../utilities/token';
 import { ITokens } from '../utilities/types';
-
-interface NavItemProps {
-  text: string;
-  icon: ReactElement;
-  path: string;
-}
+import { adminItems, mainItems, NavItem } from '../utilities/navigationConstants';
 
 interface StyledListItemButtonProps {
   active?: boolean;
@@ -81,26 +71,13 @@ const StyledListItemButton = styled(ListItemButton, {
   },
 }));
 
-// FIXME: change routes to correct route
-const mainItems = [
-  { text: 'Dashboard', icon: <HomeOutlinedIcon />, path: '/' },
-  { text: 'Kurser', icon: <ImportContactsOutlinedIcon />, path: '/courses' },
-];
-
-const adminItems = [
-  { text: 'Användare', icon: <PeopleOutlineOutlinedIcon />, path: '/admin/users' },
-  { text: 'Kurser', icon: <ImportContactsOutlinedIcon />, path: '/admin/courses' },
-  { text: 'Moduler', icon: <EventNoteOutlinedIcon />, path: '/admin/modules' },
-  { text: 'Aktiviteter', icon: <SettingsOutlinedIcon />, path: '/admin/activities' },
-];
-
 const Sidebar = (): ReactElement => {
   const navigate = useNavigate();
   const location = useLocation();
   const token = getTokens() as ITokens;
   const { role } = decodeToken(token ? token.accessToken : '');
 
-  const renderNavItems = (items: NavItemProps[]) =>
+  const renderNavItems = (items: NavItem[]) =>
     items.map(({ text, icon, path }) => (
       <ListItem key={text} disablePadding>
         <StyledListItemButton active={location.pathname.startsWith(path)} onClick={() => navigate(path)}>
