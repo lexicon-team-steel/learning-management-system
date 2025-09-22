c7a1bd0b4a923f0874113c038327dbc9aa188fc1
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-22 14:50:08 +0200

    Add Login component and style layout in LoginPage

--- Frontend/src/pages/LoginPage.tsx
+++ Frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,18 @@
-import { Container } from '@mui/material';
+import Login from '../components/Login';
+import { Container, ContainerProps, styled } from '@mui/material';
+
+const Main = styled(Container)<ContainerProps>(({ theme }) => ({
+  padding: theme.spacing(6),
+  display: 'flex',
+  justifyContent: 'center',
+}));
 
 const LoginPage = () => {
-  return <Container></Container>;
+  return (
+    <Main component="main">
+      <Login />
+    </Main>
+  );
 };
 
 export default LoginPage;
