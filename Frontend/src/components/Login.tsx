956ef2c31ab8416fb8d9e73f6494ad7ff09b38d3
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-23 11:42:39 +0200

    Use Card and Typgraphy from MUI

--- Frontend/src/components/Login.tsx
+++ Frontend/src/components/Login.tsx
@@ -1,22 +1,22 @@
 import { FormEventHandler, ReactElement, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router';
 import { useAuthContext } from '../utilities/hooks/useAuthContext';
-import { Alert, Button, styled, TextField } from '@mui/material';
+import { Alert, Button, Card, styled, TextField, Typography } from '@mui/material';
 import LoginIcon from '@mui/icons-material/Logout';
 import ImportContactsOutlinedIcon from '@mui/icons-material/ImportContactsOutlined';
 import theme from '../styles/theme';
 
-const StyledForm = styled('form')(() => ({
-  backgroundColor: theme.palette.background.paper,
-  borderRadius: theme.shape.borderRadius,
+const StyledCard = styled(Card)(() => ({
   maxWidth: '450px',
+  padding: theme.spacing(4),
+  width: '100%',
+}));
+
+const StyledForm = styled('form')(() => ({
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
   gap: theme.spacing(3),
-  padding: theme.spacing(4),
-  width: '100%',
-  boxShadow: '0 2px 3px 2px #b2b6c2ff',
 }));
 
 const StyledIcon = styled(ImportContactsOutlinedIcon)(() => ({
@@ -47,37 +47,39 @@ const Login = (): ReactElement => {
   };
 
   return (
-    <StyledForm onSubmit={handleOnSubmit}>
-      <StyledIcon color="primary" />
-      <h1>Steel LMS</h1>
-      <TextField
-        label="Användarnamn"
-        name="username"
-        variant="outlined"
-        onChange={(e) => setUsername(e.target.value)}
-        fullWidth
-        required
-      />
-      <TextField
-        label="Lösenord"
-        name="password"
-        type="password"
-        variant="outlined"
-        onChange={(e) => setPassword(e.target.value)}
-        fullWidth
-        required
-      />
-      {error && <Alert severity="error">{error}</Alert>}
-      <Button
-        type="submit"
-        variant="contained"
-        startIcon={<LoginIcon />}
-        disabled={username === '' || password === ''}
-        fullWidth
-      >
-        Logga in
-      </Button>
-    </StyledForm>
+    <StyledCard>
+      <StyledForm onSubmit={handleOnSubmit}>
+        <StyledIcon color="primary" />
+        <Typography variant="h1">Steel LMS</Typography>
+        <TextField
+          label="Användarnamn"
+          name="username"
+          variant="outlined"
+          onChange={(e) => setUsername(e.target.value)}
+          fullWidth
+          required
+        />
+        <TextField
+          label="Lösenord"
+          name="password"
+          type="password"
+          variant="outlined"
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          required
+        />
+        {error && <Alert severity="error">{error}</Alert>}
+        <Button
+          type="submit"
+          variant="contained"
+          startIcon={<LoginIcon />}
+          disabled={username === '' || password === ''}
+          fullWidth
+        >
+          Logga in
+        </Button>
+      </StyledForm>
+    </StyledCard>
   );
 };
 
