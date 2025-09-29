4b2dd248f9385326a87f8576646cbfc4ab579c90
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-29 23:53:32 +0200

    Use module data to build module summary

--- Frontend/src/pages/ModulePage.tsx
+++ Frontend/src/pages/ModulePage.tsx
@@ -1,25 +1,33 @@
 import { ReactElement, Suspense } from 'react';
-import { Await } from 'react-router';
+import { Await, useLoaderData } from 'react-router';
 import { Stack, Typography } from '@mui/material';
 import Card from '../components/Card';
 import ModuleActivities from '../components/ModuleActivities';
 import theme from '../styles/theme';
+import { IModule } from '../utilities/types';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import { formatDate } from '../utilities/helpers';
 
 const ModulePage = (): ReactElement => {
+  const { module, activities } = useLoaderData();
+
   return (
     <Stack spacing={theme.layout.gapLarge}>
       <Suspense>
-        {/* <Await> */}
-
-        <Card title="Title" titleVariant="h1">
-          <Typography>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat veritatis voluptates rem consequuntur esse
-            corporis reprehenderit. Illum quos adipisci animi nisi neque quod debitis eius quasi quibusdam at, dolorum
-            nemo.
-          </Typography>
-        </Card>
-
-        {/* </Await> */}
+        <Await resolve={module}>
+          {(module: IModule) => (
+            <Card title={module.name} titleVariant="h1">
+              <Typography mb={theme.layout.gap}>{module.description}</Typography>
+              <Stack direction="row" spacing={theme.spacing(6)}>
+                <Typography variant="body2">Del av kurs:</Typography>
+                <Typography variant="body2" display="flex" alignItems="center" gap={theme.spacing(1)}>
+                  <CalendarMonthIcon fontSize="small" />
+                  Period: {formatDate(module.startDate)} - {formatDate(module.endDate)}
+                </Typography>
+              </Stack>
+            </Card>
+          )}
+        </Await>
       </Suspense>
       <ModuleActivities />
     </Stack>
