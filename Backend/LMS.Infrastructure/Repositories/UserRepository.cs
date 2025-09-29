70749b4f284b4181026970e75bfadc0784624a9e
katja tsareva <katja.tcareva@gmail.com>
2025-09-29 16:07:27 +0200

    Implement GetAllUsersAsync function in repository

--- Backend/LMS.Infrastructure/Repositories/UserRepository.cs
+++ Backend/LMS.Infrastructure/Repositories/UserRepository.cs
@@ -1,12 +1,27 @@
 using Domain.Contracts.Repositories;
 using Domain.Models.Entities;
 using LMS.Infrastructure.Data;
+using LMS.Shared.DTOs.UserDtos;
 using Microsoft.EntityFrameworkCore;
 
 namespace LMS.Infrastructure.Repositories;
 
 public class UserRepository(ApplicationDbContext context) : RepositoryBase<ApplicationUser>(context), IUserRepository
 {
+    public Task<List<ApplicationUser>> GetAllUsersAsync(UserQueryParameters userParams)
+    {
+        var users = FindAll();
+        if (!string.IsNullOrWhiteSpace(userParams.Name))
+        {
+            var name = $"%{userParams.Name}%";
+            users = users.Where(u => EF.Functions.Like(u.LastName, name)
+                || EF.Functions.Like(u.FirstName, name));
+        }
+        if (!string.IsNullOrWhiteSpace(userParams.Role))
+            users = users.Where(u => u.UserRoles.Any(u => EF.Functions.Like(u.Role.Name, userParams.Role)));
+        return users.Include(u => u.UserRoles).ThenInclude(ur => ur.Role).ToListAsync();
+    }
+
     public Task<ApplicationUser?> GetUserAsync(string id) =>
         FindByCondition(s => s.Id == id).FirstOrDefaultAsync();
 }
