c6102188c2f8989acfbd581c671cbf5544163fba
Anton Petersson <anton.m.petersson@gmail.com>
2025-09-29 15:13:13 +0200

    helper function to check if user is teacher

--- Frontend/src/utilities/helpers.ts
+++ Frontend/src/utilities/helpers.ts
@@ -1,4 +1,22 @@
+import { redirect } from 'react-router';
+import { getTokens } from './token';
+import decodeToken from './token/decodeToken';
+
 export const formatDate = (date: string) => {
   const dateFromString = new Date(date);
   return new Intl.DateTimeFormat('sv-SE').format(dateFromString);
+};
+
+export const requireTeacherRole = () => {
+  const tokens = getTokens();
+  if (!tokens?.accessToken) {
+    throw redirect('/login');
+  }
+
+  const accessToken = decodeToken(tokens.accessToken);
+  const role = accessToken.role;
+
+  if (role !== 'Teacher') {
+    throw redirect('/notauthorized');
+  }
 };
