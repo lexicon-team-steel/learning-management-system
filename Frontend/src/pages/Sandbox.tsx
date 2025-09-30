6f6ca78168f141d3cf841801e82f2ef5573e3fd5
katja tsareva <katja.tcareva@gmail.com>
2025-09-30 14:38:29 +0200

    Added component to sandbox

--- Frontend/src/pages/Sandbox.tsx
+++ Frontend/src/pages/Sandbox.tsx
@@ -1,10 +1,21 @@
 import Main from '../components/Main';
-import MuduleActivities from '../components/ModuleActivities';
+import Table from '../components/Table';
+import { IParticipant } from '../utilities/types';
+import UserTableRow from '../components/UserTableRow';
+import { mockUsers } from '../utilities/data/mockData';
 
 const Sandbox = () => {
+  const handleAction = (user: IParticipant) => console.log(user);
   return (
     <Main>
-      <MuduleActivities /> {/*  // temp for testing */}
+      <Table
+        headers={['Namn', 'E-post', 'Roll', 'Åtgärder']}
+        keyField="id"
+        rows={mockUsers}
+        renderItem={(user: IParticipant) => (
+          <UserTableRow user={user} onEdit={() => handleAction(user)} onDelete={() => handleAction(user)} />
+        )}
+      ></Table>
     </Main>
   );
 };
