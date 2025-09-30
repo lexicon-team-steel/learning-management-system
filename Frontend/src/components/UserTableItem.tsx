a1c41b9a8fafd203db77521eb10f96f4225f391f
katja tsareva <katja.tcareva@gmail.com>
2025-09-30 13:44:27 +0200

    Add icon buttons to user table item

--- Frontend/src/components/UserTableItem.tsx
+++ Frontend/src/components/UserTableItem.tsx
@@ -1,12 +1,16 @@
-import { Chip, TableCell, TableRow } from '@mui/material';
+import { Chip, IconButton, TableCell, TableRow } from '@mui/material';
 import { ReactElement } from 'react';
 import { IParticipant } from '../utilities/types';
 import EmailLink from './EmailLink';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import EditIcon from '@mui/icons-material/EditOutlined';
 
 interface IUserItem {
   user: IParticipant;
+  onEdit: () => void;
+  onDelete: () => void;
 }
-const UserTableItem = ({ user }: IUserItem): ReactElement => {
+const UserTableItem = ({ user, onEdit, onDelete }: IUserItem): ReactElement => {
   const userName = user.lastName + '' + user.firstName;
   const userRole = user.role[0];
   const chipColor = userRole === 'Student' ? 'primary' : 'secondary';
@@ -20,7 +24,14 @@ const UserTableItem = ({ user }: IUserItem): ReactElement => {
       <TableCell>
         <Chip label={userRole} color={chipColor} />
       </TableCell>
-      <TableCell></TableCell>
+      <TableCell>
+        <IconButton onClick={onEdit} aria-label="edit">
+          <EditIcon fontSize="small" color="primary" />
+        </IconButton>
+        <IconButton onClick={onDelete} aria-label="delete">
+          <DeleteIcon fontSize="small" color="error" />
+        </IconButton>
+      </TableCell>
     </TableRow>
   );
 };
