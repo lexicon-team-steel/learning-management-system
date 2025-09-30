78c82f5b8dd77405b5f778c8e0e312eac3a3919b
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-09-30 12:52:25 +0200

    Use Date component for period

--- Frontend/src/pages/ModulePage.tsx
+++ Frontend/src/pages/ModulePage.tsx
@@ -5,8 +5,8 @@ import Card from '../components/Card';
 import ModuleActivities from '../components/ModuleActivities';
 import theme from '../styles/theme';
 import { IActivity, IModule } from '../utilities/types';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import { formatDate } from '../utilities/helpers';
+import Date from '../components/Date';
 
 const ModulePage = (): ReactElement => {
   const { module, activities } = useLoaderData();
@@ -20,10 +20,7 @@ const ModulePage = (): ReactElement => {
               <Typography mb={theme.layout.gap}>{module.description}</Typography>
               <Stack direction="row" spacing={theme.spacing(6)}>
                 <Typography variant="body2">Del av kurs:</Typography>
-                <Typography variant="body2" display="flex" alignItems="center" gap={theme.spacing(1)}>
-                  <CalendarMonthIcon fontSize="small" />
-                  Period: {formatDate(module.startDate)} - {formatDate(module.endDate)}
-                </Typography>
+                <Date start={formatDate(module.startDate)} end={formatDate(module.endDate)} />
               </Stack>
             </Card>
           )}
