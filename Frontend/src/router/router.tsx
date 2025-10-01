d96a20f3ed6a3303557795ff6e74c3a1e0fd2f3e
wpplng <lina.wappling@gmail.com>
2025-10-01 12:00:47 +0200

    Removed route for courses

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -8,9 +8,7 @@ import { dashboardLoader } from '../utilities/loaders/dashboardLoader';
 import CoursePage from '../pages/CoursePage';
 import { moduleLoader } from '../utilities/loaders/moduleLoader';
 import { courseLoader, myCourseLoader } from '../utilities/loaders/courseLoader';
-import { allCoursesLoader } from '../utilities/loaders/allCoursesLoader';
 import NotAuthorized from '../pages/NotAuthorized';
-import CoursesPage from '../pages/CoursesPage';
 import ModulePage from '../pages/ModulePage';
 import { adminUsersLoader } from '../utilities/loaders/adminUsersLoader';
 import AdminUsersPage from '../pages/AdminUsersPage';
@@ -34,11 +32,6 @@ export const router = createBrowserRouter([
       {
         path: '*',
         element: <Sandbox />,
-      },
-      {
-        path: 'courses',
-        element: <CoursesPage />,
-        loader: allCoursesLoader,
       },
       {
         path: 'courses/:id',
