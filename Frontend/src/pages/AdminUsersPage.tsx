b040a8a218df795cd1794cc5b2d4aad65765f3f5
katja tsareva <katja.tcareva@gmail.com>
2025-10-01 10:49:59 +0200

    Moved AdminPageTitle to a separate file

--- Frontend/src/pages/AdminUsersPage.tsx
+++ Frontend/src/pages/AdminUsersPage.tsx
@@ -1,28 +1,13 @@
 import { ReactElement } from 'react';
 import { useLoaderData } from 'react-router';
 import { IParticipant } from '../utilities/types';
-import { Box, Button, Stack, Typography } from '@mui/material';
+import { Stack } from '@mui/material';
 import theme from '../styles/theme';
-import PlusIcon from '@mui/icons-material/Add';
+
 import UserTable from '../components/UserTable';
 import UserForm from '../components/UserForm';
 import { useCrud } from '../utilities/hooks/useCrud';
-
-interface IAdminProps {
-  pageTitle: string;
-  buttonLabel: string;
-  buttonDisabled: boolean;
-  onButtonClick: () => void;
-}
-
-const AdminTitle = ({ pageTitle, buttonLabel, buttonDisabled, onButtonClick }: IAdminProps): ReactElement => (
-  <Box display="flex" justifyContent="space-between" alignItems="center" gap={theme.layout.gap}>
-    <Typography variant="h1">{pageTitle}</Typography>
-    <Button startIcon={<PlusIcon />} variant="contained" disabled={buttonDisabled} onClick={onButtonClick}>
-      {buttonLabel}
-    </Button>
-  </Box>
-);
+import AdminPageTitle from '../components/AdminPageTitle';
 
 const AdminUsersPage = (): ReactElement => {
   const { users } = useLoaderData();
@@ -32,14 +17,14 @@ const AdminUsersPage = (): ReactElement => {
 
   return (
     <Stack spacing={theme.layout.gapLarge}>
-      <AdminTitle
+      <AdminPageTitle
         pageTitle="Hantera användare"
         buttonLabel="Skapa ny användare"
         buttonDisabled={isEditing}
         onButtonClick={() => handleChange({ id: '', lastName: '', firstName: '', email: '', roles: [] })}
       />
       {selectedItem && <UserForm user={selectedItem} onSubmit={handleSave} onCancel={handleCancel} />}
-      <UserTable users={items} onEdit={handleChange} onDelete={handleDelete} />;
+      <UserTable users={items} onEdit={handleChange} onDelete={handleDelete} />
     </Stack>
   );
 };
