e7b07c6bab9c1f98561ff68779c3c8a83560f322
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-10-02 17:43:39 +0200

    Add CourseTable

---
+++ Frontend/src/components/CourseTable.tsx
@@ -0,0 +1,23 @@
+import { ReactElement } from 'react';
+import Table from './Table';
+import { ICourse } from '../utilities/types';
+import CourseTableRow from './CourseTableRow';
+
+interface ICourseTableProps {
+  courses: ICourse[];
+  onEdit: (course: ICourse) => void;
+  onDelete: (course: ICourse) => void;
+}
+
+const CourseTable = ({ courses, onEdit, onDelete }: ICourseTableProps): ReactElement => (
+  <Table
+    headers={['Namn', 'Period']}
+    keyField="id"
+    rows={courses}
+    renderItem={(course: ICourse) => (
+      <CourseTableRow course={course} onEdit={() => onEdit(course)} onDelete={() => onDelete(course)} />
+    )}
+  ></Table>
+);
+
+export default CourseTable;
