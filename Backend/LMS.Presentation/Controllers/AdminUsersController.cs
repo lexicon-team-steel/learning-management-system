6bb93788b27ab5271bc60e9bef25c40a92be0eb5
katja tsareva <katja.tcareva@gmail.com>
2025-10-03 00:51:41 +0200

    Return json on create

--- Backend/LMS.Presentation/Controllers/AdminUsersController.cs
+++ Backend/LMS.Presentation/Controllers/AdminUsersController.cs
@@ -15,27 +15,29 @@ namespace LMS.Presentation.Controllers;
 [Authorize(Roles = "Teacher")]
 public class AdminUsersController(IServiceManager serviceManager) : ControllerBase
 {
-        [HttpGet]
-        [SwaggerOperation(
-                Summary = "Get all users",
-                Description = "Returns users for admin. Can be filtered by name or role")]
-        [SwaggerResponse(StatusCodes.Status200OK, "List of user", typeof(IEnumerable<UserDto>))]
-        [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
-        [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
-        public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers([FromQuery] UserQueryParameters userParams) =>
-            Ok(await serviceManager.UserService.GetAllUsersAsync(userParams));
+    [HttpGet]
+    [SwaggerOperation(
+            Summary = "Get all users",
+            Description = "Returns users for admin. Can be filtered by name or role")]
+    [SwaggerResponse(StatusCodes.Status200OK, "List of user", typeof(IEnumerable<UserDto>))]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
+    [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
+    public async Task<ActionResult<IEnumerable<UserDto>>> GetAllUsers([FromQuery] UserQueryParameters userParams) =>
+        Ok(await serviceManager.UserService.GetAllUsersAsync(userParams));
 
-        [HttpPost]
-        [SwaggerOperation(
-                Summary = "Create user",
-                Description = "Creates a new user account with the provided registration details")]
-        [SwaggerResponse(StatusCodes.Status201Created, "User successfully created")]
-        [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid input or registration failed")]
-        [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
-        [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
-        public async Task<ActionResult> CreateUser(UserRegistrationDto userRegistrationDto)
-        {
-                IdentityResult result = await serviceManager.AuthService.RegisterUserAsync(userRegistrationDto);
-                return result.Succeeded ? StatusCode(StatusCodes.Status201Created) : BadRequest(new { errors = result.ToErrorDictionary() });
-        }
+    [HttpPost]
+    [SwaggerOperation(
+            Summary = "Create user",
+            Description = "Creates a new user account with the provided registration details")]
+    [SwaggerResponse(StatusCodes.Status201Created, "User successfully created")]
+    [SwaggerResponse(StatusCodes.Status400BadRequest, "Invalid input or registration failed")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
+    [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
+    public async Task<ActionResult> CreateUser(UserRegistrationDto userRegistrationDto)
+    {
+        IdentityResult result = await serviceManager.AuthService.RegisterUserAsync(userRegistrationDto);
+        return result.Succeeded
+                ? CreatedAtAction(nameof(CreateUser), new { status = "ok" })
+                : BadRequest(new { errors = result.ToErrorDictionary() });
+    }
 }
