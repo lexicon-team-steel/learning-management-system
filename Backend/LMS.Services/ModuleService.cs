ba8d7392c6ecfb2efcbac41a35d29c29513314c0
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-10-03 15:42:21 +0200

    Add CreateModuleAsync method

--- Backend/LMS.Services/ModuleService.cs
+++ Backend/LMS.Services/ModuleService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Domain.Contracts.Repositories;
+using Domain.Models.Entities;
 using Domain.Models.Exceptions;
 using LMS.Shared.DTOs.CourseModuleDtos;
 using Service.Contracts;
@@ -16,5 +17,25 @@ public class ModuleService(IMapper mapper, IUnitOfWork uow, ICurrentUserService 
         if (module == null) throw new NotFoundException("Module not found or you don't have access");
 
         return mapper.Map<CourseModuleDto>(module);
+    }
+
+    public async Task<CourseModuleDto> CreateModuleAsync(Guid courseId, CreateModuleDto dto)
+    {
+        var exists = await uow.Modules.ExistsByNameAsync(courseId, dto.Name);
+        if (exists)
+            throw new ConflictException($"A course with the name '{dto.Name}' already exists.");
+
+        if (dto.EndDate < dto.StartDate)
+        {
+            throw new BadRequestException("End date cannot be earlier than start date");
+        }
+
+        var module = mapper.Map<CourseModule>(dto);
+
+        uow.Modules.Create(module);
+        await uow.CompleteAsync();
+
+        return mapper.Map<CourseModuleDto>(module);
+
     }
 }
