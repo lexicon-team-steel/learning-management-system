4a939510c83239749318bfc981179941d2158821
Anton Petersson <anton.m.petersson@gmail.com>
2025-10-06 11:17:36 +0200

    fixed imports after renaming

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from '@emotion/react';
 import theme from './styles/theme';
 import { CssBaseline } from '@mui/material';
 import { AlertProvider } from './utilities/context/alert/AlertProvider';
-import { CoursesProvider } from './utilities/context/course/coursesProvider';
-import { AuthProvider } from './utilities/context/auth/authProvider';
+import { CoursesProvider } from './utilities/context/course/CoursesProvider';
+import { AuthProvider } from './utilities/context/auth/AuthProvider';
 
 createRoot(document.getElementById('root')!).render(
   <AuthProvider>
