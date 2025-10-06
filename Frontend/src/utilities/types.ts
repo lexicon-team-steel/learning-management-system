9ba2468b014e8e5a1dd9b23c2c576bf4193af854
wpplng <lina.wappling@gmail.com>
2025-10-06 12:45:32 +0200

    Removed duble types and added type for modules loader

--- Frontend/src/utilities/types.ts
+++ Frontend/src/utilities/types.ts
@@ -67,15 +67,6 @@ export interface IActivityType {
   name: string;
 }
 
-export interface IModule {
-  id: string;
-  name: string;
-  description: string;
-  startDate: string;
-  endDate: string;
-  activities: IActivity[];
-}
-
 export interface ICourse {
   id: string;
   name: string;
@@ -85,9 +76,8 @@ export interface ICourse {
   modules?: IModule[];
 }
 
-export interface ICourseLoader {
-  course: Promise<ICourse>;
-  participants: Promise<IParticipant[]>;
+export interface IModulesLoader {
+  modules: Promise<IModule[]>;
 }
 
 export interface ICoursesLoader {
