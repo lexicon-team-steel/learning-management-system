57e3c6858c44d0f1866cd5b3ead3bd622136af3f
katja tsareva <katja.tcareva@gmail.com>
2025-10-06 21:35:36 +0200

    Add admin service update method for users

--- Backend/Service.Contracts/IUserService.cs
+++ Backend/Service.Contracts/IUserService.cs
@@ -1,9 +1,11 @@
 using LMS.Shared.DTOs.UserDtos;
 using LMS.Shared.Parameters;
+using Microsoft.AspNetCore.Identity;
 
 namespace Service.Contracts;
 
 public interface IUserService
 {
     Task<IEnumerable<UserDto>> GetAllUsersAsync(UserQueryParameters userParams);
+    Task<IdentityResult> UpdateUserAsync(string id, UserUpdateDto updateDto);
 }
