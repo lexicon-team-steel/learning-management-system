8d98dcb1cf78252f6cc2cb5123af8664e882d18c
wpplng <lina.wappling@gmail.com>
2025-10-06 13:33:37 +0200

    Using adminCoursesAction on route

--- Frontend/src/router/router.tsx
+++ Frontend/src/router/router.tsx
@@ -56,6 +56,7 @@ export const router = createBrowserRouter([
         path: 'admin/courses',
         element: <AdminCoursesPage />,
         loader: adminCoursesLoader,
+        action: adminUsersAction,
       },
       /* ---- Add new routes above this comment for a neater structure ---- */
       { path: 'notauthorized', element: <NotAuthorizedPage /> },
