ad37c1fe19a8c57fb7d59112076ae66bfe16eba2
Anton Petersson <anton.m.petersson@gmail.com>
2025-10-06 14:14:11 +0200

    added validation so endDate won't be before startDate

--- Frontend/src/utilities/actions/adminCoursesAction.ts
+++ Frontend/src/utilities/actions/adminCoursesAction.ts
@@ -1,29 +1,41 @@
 import { ActionFunctionArgs } from 'react-router';
 import { FormErrorType, IBasicAction } from '../types';
 
-const validateCourse = (formData: FormData, action: string): IBasicAction => {
+const validateCourse = (formData: FormData): IBasicAction => {
+
   const name = formData.get('name') as string;
   const description = formData.get('description') as string;
-  const startDate = formData.get('startDate') as string;
-  const endDate = formData.get('endDate') as string;
+  const startDateStr = formData.get('startDate') as string;
+  const endDateStr = formData.get('endDate') as string;
 
   const errors: FormErrorType = {};
 
-  if (!name) errors.name = 'Förnamn är obligatoriskt';
-  if (!description) errors.description = 'Efternamn är obligatoriskt';
-  if (!startDate) errors.startDate = 'Startdatum är obligatoriskt';
-  if (!endDate) errors.endDate = 'Slutdatum är obligatoriskt';
- 
+  if (!name) errors.name = 'Titel är obligatoriskt';
+  if (!description) errors.description = 'Beskrivning är obligatorisk';
+  if (!startDateStr) errors.startDate = 'Startdatum är obligatoriskt';
+  if (!endDateStr) errors.endDate = 'Slutdatum är obligatoriskt';
 
+  if (startDateStr && endDateStr) {
+    const startDate = new Date(startDateStr);
+    const endDate = new Date(endDateStr);
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+    startDate.setHours(0, 0, 0, 0);
+    endDate.setHours(0, 0, 0, 0);
+
+    if (startDate > endDate) {
+      errors.endDate = 'Slutdatum måste vara efter startdatum';
+    }
+  }
 
   return errors;
 };
 
 export const adminCoursesAction = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData();
-  console.log(Object.fromEntries(formData));
 
-  const errors = validateCourse(formData, formData.get('_action') as string);
+  const errors = validateCourse(formData);
 
   if (Object.keys(errors).length > 0) {
     return { errors };
