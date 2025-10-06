3ff97246e25eb4b841a0ee3c9fd04deb4dcc5e57
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-10-06 22:37:28 +0200

    Implement ModuleForm on AdminModulesPage

--- Frontend/src/pages/AdminModulesPage.tsx
+++ Frontend/src/pages/AdminModulesPage.tsx
@@ -1,16 +1,20 @@
-import { Form, useLoaderData } from 'react-router';
+import { useLoaderData } from 'react-router';
 import ModuleTable from '../components/ModuleTable';
 import { useCallback } from 'react';
 import { IForm, IModule, ITable } from '../utilities/types';
 import AdminCrudPage from '../components/AdminCrudPage';
 import { EMPTY_MODULE } from '../utilities/constants';
+import ModuleForm from '../components/ModuleForm';
 
 const AdminModulesPage = () => {
   const { courseWithModules } = useLoaderData();
   const modules = courseWithModules.modules;
   const courseName = courseWithModules.name;
 
-  const FormComponent = useCallback(({ item }: IForm<IModule>) => <Form>{item.name}</Form>, []);
+  const FormComponent = useCallback(
+    ({ item, onCancel, errors }: IForm<IModule>) => <ModuleForm module={item} onCancel={onCancel} errors={errors} />,
+    []
+  );
 
   const TableComponent = useCallback(
     ({ items, onEdit, onDelete }: ITable<IModule>) => (
