dae3d13facc0174eb761cea41cd2a727437d2db4
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-10-06 22:50:27 +0200

    Use TextInput in ModuleForm

--- Frontend/src/components/ModuleForm.tsx
+++ Frontend/src/components/ModuleForm.tsx
@@ -1,42 +1,17 @@
 import { ReactElement, useState } from 'react';
 import { FormErrorType, IModule } from '../utilities/types';
-import { Grid, TextField } from '@mui/material';
+import { Grid } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import theme from '../styles/theme';
 import AdminPageForm from './AdminPageForm';
 import dayjs, { Dayjs } from 'dayjs';
+import TextInput from './TextInput';
 
 interface IModuleFormProps {
   onCancel: () => void;
   module: IModule;
   errors?: FormErrorType;
 }
-
-interface IStyledTextfield {
-  type?: string;
-  label: string;
-  name: string;
-  value: string;
-  error?: string;
-  required?: boolean;
-  rows?: number;
-}
-
-const StyledTextField = ({ type = 'text', label, name, value, required = true, error, rows }: IStyledTextfield) => (
-  <TextField
-    type={type}
-    label={label}
-    name={name}
-    variant="outlined"
-    defaultValue={value}
-    fullWidth
-    required={required}
-    error={!!error}
-    helperText={error}
-    rows={rows}
-    multiline={rows ? true : false}
-  />
-);
 
 const ModuleForm = ({ onCancel, module, errors }: IModuleFormProps): ReactElement => {
   const action = module.id ? 'edit' : 'create';
@@ -54,14 +29,14 @@ const ModuleForm = ({ onCancel, module, errors }: IModuleFormProps): ReactElemen
       <input type="hidden" name="endDate" value={endDate?.isValid() ? endDate.format('YYYY-MM-DD') : ''} />
       <Grid container spacing={theme.layout.gap}>
         <Grid size={6}>
-          <StyledTextField label="Titel" name="name" value={module.name} error={errors?.name} />
+          <TextInput label="Titel" name="name" value={module.name} error={errors?.name} />
         </Grid>
         <Grid size={12}>
-          <StyledTextField
+          <TextInput
             type="text"
             label="Beskrivning"
             name="description"
-            rows={3}
+            minRows={3}
             value={module.description}
             error={errors?.description}
           />
