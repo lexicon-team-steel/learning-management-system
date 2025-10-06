a2f63fb63d159a532fc59bc9904d01fc55b1409f
Malin Johansson <malin.johansson@medieinstitutet.se>
2025-10-06 11:12:25 +0200

    Move POST module to AdminCourseController

--- Backend/LMS.Presentation/Controllers/AdminCoursesController.cs
+++ Backend/LMS.Presentation/Controllers/AdminCoursesController.cs
@@ -1,4 +1,5 @@
 using LMS.Shared.DTOs.CourseDtos;
+using LMS.Shared.DTOs.CourseModuleDtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,17 +13,19 @@ namespace LMS.Presentation.Controllers;
 [Authorize(Roles = "Teacher")]
 public class AdminCoursesController(IServiceManager serviceManager) : ControllerBase
 {
-    private readonly ICourseService courseService = serviceManager.CourseService;
+        private readonly ICourseService courseService = serviceManager.CourseService;
+        private readonly IModuleService moduleService = serviceManager.ModuleService;
 
-    [HttpGet]
-    [SwaggerOperation(
-            Summary = "Get all courses",
-            Description = "Returns courses for admin.")]
-    [SwaggerResponse(StatusCodes.Status200OK, "List of courses", typeof(IEnumerable<CourseDto>))]
-    [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
-    [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
-    public async Task<ActionResult<IEnumerable<CourseDto>>> GetAllCourses() =>
-        Ok(await courseService.GetAllCoursesAsync());
+
+        [HttpGet]
+        [SwaggerOperation(
+                Summary = "Get all courses",
+                Description = "Returns courses for admin.")]
+        [SwaggerResponse(StatusCodes.Status200OK, "List of courses", typeof(IEnumerable<CourseDto>))]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - You do not have permission to access this resource.")]
+        public async Task<ActionResult<IEnumerable<CourseDto>>> GetAllCourses() =>
+            Ok(await courseService.GetAllCoursesAsync());
 
         [HttpPost]
         [SwaggerOperation(
@@ -37,6 +40,21 @@ public class AdminCoursesController(IServiceManager serviceManager) : Controller
                 var course = await courseService.CreateAsync(dto);
 
                 return CreatedAtAction(nameof(CreateCourse), new { status = "ok" }, course);
+        }
+
+        [HttpPost("{courseId}/modules")]
+        [SwaggerOperation(
+                Summary = "Create a new module",
+                Description = "Creates a new module in a course. Only teachers are allowed.")]
+        [SwaggerResponse(StatusCodes.Status201Created, "Module created", typeof(CourseModuleDto))]
+        [SwaggerResponse(StatusCodes.Status400BadRequest, "Validation failed (e.g. endDate < startDate)")]
+        [SwaggerResponse(StatusCodes.Status401Unauthorized, "Unauthorized - JWT token missing or invalid")]
+        [SwaggerResponse(StatusCodes.Status403Forbidden, "Forbidden - only teachers can create courses")]
+        public async Task<ActionResult<CourseModuleDto>> CreateModule(Guid courseId, [FromBody] CreateModuleDto dto)
+        {
+                var module = await moduleService.CreateModuleAsync(courseId, dto);
+
+                return CreatedAtAction(nameof(CreateModule), new { status = "ok" }, module);
         }
 
 }
