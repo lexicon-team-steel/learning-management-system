d6d7dbce67f84db38e58515f64490cb4e08f0cf9
wpplng <lina.wappling@gmail.com>
2025-10-07 13:33:47 +0200

    adminModulesAction works

--- Frontend/src/utilities/actions/adminModulesAction.ts
+++ Frontend/src/utilities/actions/adminModulesAction.ts
@@ -3,10 +3,10 @@ import { adminEntityAction } from './adminEntityAction';
 import { ActionFunctionArgs } from 'react-router';
 import { validateNameDescriptionAndDates } from './helpers/validationHelpers';
 
-export const adminModulesAction = ({ params }: ActionFunctionArgs) =>
+export const adminModulesAction = (args: ActionFunctionArgs) =>
   adminEntityAction({
     entity: 'module',
     validate: validateNameDescriptionAndDates,
-    apiURL: `${BASE_URL}/admin/courses/${params.courseId}/modules`,
-    redirectURL: `/admin/courses/${params.courseId}`,
-  });
+    apiURL: `${BASE_URL}/admin/courses/${args.params.courseId}/modules`,
+    redirectURL: `/admin/courses/${args.params.courseId}`,
+  })(args);
