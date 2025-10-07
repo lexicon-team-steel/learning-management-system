657843c7c7fafb6b4caa721b0765df23d83d574d
wpplng <lina.wappling@gmail.com>
2025-10-07 13:13:19 +0200

    Use helpers and changed use of params

--- Frontend/src/utilities/actions/adminModulesAction.ts
+++ Frontend/src/utilities/actions/adminModulesAction.ts
@@ -1,39 +1,12 @@
-import { FormErrorType } from '../types';
 import { BASE_URL } from '../constants';
 import { adminEntityAction } from './adminEntityAction';
+import { ActionFunctionArgs } from 'react-router';
+import { validateNameDescriptionAndDates } from './helpers/validationHelpers';
 
-const validateModule = (formData: FormData): FormErrorType => {
-  const title = formData.get('title') as string;
-  const description = formData.get('description') as string;
-  const startDate = formData.get('startDate') as string;
-  const endDate = formData.get('endDate') as string;
-
-  const errors: FormErrorType = {};
-
-  if (!title) errors.title = 'Title är obligatoriskt';
-  if (!description) errors.description = 'Beskrivning är obligatoriskt';
-
-  if (!startDate) errors.startDate = 'Startdatum är obligatoriskt';
-  if (!endDate) errors.endDate = 'Slutdatum är obligatoriskt';
-
-  if (startDate && endDate) {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-
-    if (isNaN(start.getTime()) || isNaN(end.getTime())) {
-      errors.startDate = 'Ogiltigt datumformat';
-    } else if (end < start) {
-      errors.endDate = 'Slutdatum får inte vara före startdatum';
-    }
-  }
-
-  return errors;
-};
-
-export const adminModulesAction = (courseId: string) =>
+export const adminModulesAction = ({ params }: ActionFunctionArgs) =>
   adminEntityAction({
     entity: 'module',
-    validate: validateModule,
-    apiURL: `${BASE_URL}/admin/courses/${courseId}/modules`,
-    redirectURL: `/admin/courses/${courseId}/modules`,
+    validate: validateNameDescriptionAndDates,
+    apiURL: `${BASE_URL}/admin/courses/${params.courseId}/modules`,
+    redirectURL: `/admin/courses/${params.courseId}`,
   });
