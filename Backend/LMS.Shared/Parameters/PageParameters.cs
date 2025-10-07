66617370fe85ef93ffe415bcbdf2b5da6905e9cb
katja tsareva <katja.tcareva@gmail.com>
2025-10-07 14:34:44 +0200

    Add Page Parameters and extend User Parameters

---
+++ Backend/LMS.Shared/Parameters/PageParameters.cs
@@ -0,0 +1,18 @@
+namespace LMS.Shared.Parameters;
+
+public class PageParameters
+{
+    private const int PageSizeMax = 100;
+    private const int PageSizeDefault = 5;
+
+    private int _pageSize = PageSizeDefault;
+    public int PageSize
+    {
+        get => _pageSize;
+        set =>
+            _pageSize = value > PageSizeMax
+            ? PageSizeMax
+            : value <= 0 ? PageSizeDefault : value;
+    }
+    public int PageIndex { get; set; } = 1;
+}
