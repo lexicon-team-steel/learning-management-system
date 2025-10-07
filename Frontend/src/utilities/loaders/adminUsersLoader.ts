a3520e089fc20411cc4fd0b8f406511f32fdaa27
katja tsareva <katja.tcareva@gmail.com>
2025-10-07 16:18:17 +0200

    Updated admin user loader to set page index and size

--- Frontend/src/utilities/loaders/adminUsersLoader.ts
+++ Frontend/src/utilities/loaders/adminUsersLoader.ts
@@ -1,11 +1,18 @@
+import { LoaderFunctionArgs } from 'react-router';
 import { fetchWithToken } from '../api/fetchWithToken';
 import { BASE_URL } from '../constants';
 import { requireTeacherRole } from '../helpers';
 import { IPagedLoader, IParticipant } from '../types';
 
-export const adminUsersLoader = async (): Promise<IPagedLoader<IParticipant>> => {
+export const adminUsersLoader = async ({ request }: LoaderFunctionArgs): Promise<IPagedLoader<IParticipant>> => {
   requireTeacherRole();
 
-  const response = (await fetchWithToken(`${BASE_URL}/admin/users`)) as IPagedLoader<IParticipant>;
+  const url = new URL(request.url);
+  const pageIndex = Number(url.searchParams.get('pageIndex')) || 1;
+  const pageSize = Number(url.searchParams.get('pageSize')) || 5;
+
+  const response = (await fetchWithToken(
+    `${BASE_URL}/admin/users?pageIndex=${pageIndex}&pageSize=${pageSize}`
+  )) as IPagedLoader<IParticipant>;
   return { ...response };
 };
