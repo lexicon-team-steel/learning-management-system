e850ea7f4e2be19a571557f47e368545c92074b9
katja tsareva <katja.tcareva@gmail.com>
2025-10-08 16:09:47 +0200

    Add Participants form with participants

---
+++ Frontend/src/components/ParticipantForm.tsx
@@ -0,0 +1,34 @@
+import { ReactElement } from 'react';
+import { FormControl, Grid, InputLabel, MenuItem, Select } from '@mui/material';
+import theme from '../styles/theme';
+import AdminPageForm from './AdminPageForm';
+import { IParticipant } from '../utilities/types';
+
+interface IParticipantFormProps {
+  onCancel: () => void;
+  users: IParticipant[];
+}
+
+const ParticipantForm = ({ onCancel, users }: IParticipantFormProps): ReactElement => {
+  return (
+    <AdminPageForm title="Lägg till en deltagare" submitLabel="Spara" onCancel={onCancel}>
+      <input type="hidden" name="_action" value="create" />
+      <input type="hidden" name="id" value="" />
+      <Grid container spacing={theme.layout.gap}>
+        <Grid size={12}>
+          <FormControl fullWidth size="small">
+            <InputLabel id="participant-label">Användare</InputLabel>
+            <Select labelId="role-label" id="role-select" label="Deltagare" defaultValue="">
+              <MenuItem value="">- Välj -</MenuItem>
+              {users.map((user) => (
+                <MenuItem key={user.id} value={user.id}>{`${user.firstName} ${user.lastName}`}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+      </Grid>
+    </AdminPageForm>
+  );
+};
+
+export default ParticipantForm;
--- Frontend/src/pages/AdminParticipantsPage.tsx
+++ Frontend/src/pages/AdminParticipantsPage.tsx
@@ -1,16 +1,20 @@
 import { useCallback } from 'react';
 import AdminCrudPage from '../components/AdminCrudPage';
 import { EMPTY_PARTICIPANT } from '../utilities/constants';
-import { IAdminParticipantsLoader, IParticipant, ITable } from '../utilities/types';
+import { IAdminParticipantsLoader, IForm, IParticipant, ITable } from '../utilities/types';
 import ParticipantsTable from '../components/ParticipantTable';
 import { useLoaderData } from 'react-router';
+import ParticipantForm from '../components/ParticipantForm';
 
 const AdminParticipantsPage = () => {
-  const { courseWithParticipants } = useLoaderData<IAdminParticipantsLoader>();
+  const { courseWithParticipants, pagedResult } = useLoaderData<IAdminParticipantsLoader>();
   const courseName = courseWithParticipants.name;
   const participants = courseWithParticipants.participants || [];
 
-  const FormComponent = useCallback(() => <div></div>, []);
+  const FormComponent = useCallback(
+    ({ onCancel }: IForm<IParticipant>) => <ParticipantForm users={pagedResult.items} onCancel={onCancel} />,
+    [pagedResult]
+  );
 
   const TableComponent = useCallback(
     ({ items, onDelete }: ITable<IParticipant>) => <ParticipantsTable participants={items} onDelete={onDelete} />,
