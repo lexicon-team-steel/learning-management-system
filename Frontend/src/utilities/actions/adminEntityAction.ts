c4a5a0a58b9e21af53c82bdd42fcb0b63912cd1f
Anton Petersson <anton.m.petersson@gmail.com>
2025-10-08 12:37:08 +0200

    added a "merge" between date and times before fetch

--- Frontend/src/utilities/actions/adminEntityAction.ts
+++ Frontend/src/utilities/actions/adminEntityAction.ts
@@ -21,21 +21,41 @@ export const adminEntityAction =
     if (Object.keys(errors).length > 0) {
       return { ...response, errors: { fieldErrors: errors } };
     }
+    const date = formData.get('date') as string | null;
+    const startTime = formData.get('startTime') as string | null;
+    const endTime = formData.get('endTime') as string | null;
 
+    if (date && startTime && endTime) {
+      const startDate = `${date}T${startTime}`;
+      const endDate = `${date}T${endTime}`;
+      formData.set('startDate', startDate);
+      formData.set('endDate', endDate);
+      formData.delete('startTime');
+      formData.delete('endTime');
+      formData.delete('date');
+    }
     switch (actionType) {
       case 'create': {
         const body = Object.fromEntries(formData.entries());
+        console.log('fetch?');
+        console.log(body);
+
         const errorResult = await safeFetch(apiURL, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(body),
         });
+        console.log(errorResult);
+
         if (errorResult) return { ...response, ...errorResult };
+
         break;
       }
+
       default:
         throw new Error(`Unknown action: ${actionType}`);
     }
+
     redirect(redirectURL);
     return { ...response, success: true };
   };
